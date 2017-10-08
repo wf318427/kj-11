2286c28411a505a60ac1896ebf198b6deb929900
zhangfei <1530181651@qq.com>
2017-10-08 20:15:12 +0800

    提交测试父类testBase

---
+++ 00.model/src/test/java/cn/tedu/test/TestBase.java
@@ -0,0 +1,13 @@
+package cn.tedu.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Administrator on 17.10.8.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestBase {
+}
