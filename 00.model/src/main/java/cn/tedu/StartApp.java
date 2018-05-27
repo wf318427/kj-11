e82349bf64136f151511a07552d3d2cf259cf5e4
zhangfei <1530181651@qq.com>
2018-05-27 21:59:21 +0800

    全部提交

--- 00.model/src/main/java/cn/tedu/StartApp.java
+++ 00.model/src/main/java/cn/tedu/StartApp.java
@@ -13,6 +13,7 @@ import javax.jms.Queue;
 @SpringBootApplication
 public class StartApp {
     public static void main(String[] args) {
+
         SpringApplication.run(StartApp.class,args);
     }
     @Bean
